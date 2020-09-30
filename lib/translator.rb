56eeb5e38784ab91982f1cb68f0921eb399c0c7d
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-30 21:20:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,8 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emotion)
-  load_library(file_path)
+  library = load_library(file_path)
+  library.
 end
 
 def get_english_meaning(file_path, emoticon)
