a1386308cabfb54a2cb89e21c0bb4b4e2c4fd353
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-30 21:24:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 def get_japanese_emoticon(file_path, emotion)
   library = load_library(file_path)
   emoticon = library.keys.find do |emo|
-    library[key][:english] == emoticon
+    library[emo][:english] == emoticon
   end 
   emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
 end
